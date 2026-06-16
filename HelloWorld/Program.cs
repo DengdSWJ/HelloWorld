c506a27d78d150b536bf76e318726da6e8fd5e81
DengdSWJ <1011768402@qq.com>
2026-06-16 16:35:18 +0800

    添加项目文件。

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWorld
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /*我的第一个C# 程序*/
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
